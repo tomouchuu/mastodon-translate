e8a5d325e5f495042aa3ca3733c493ea1eaa8bd2
Thomas Moore <tomo@uchuu.io>
2017-04-17 08:33:25 +0100

    Add options to the preferences area of mastodon to choose what language to translate to, close #1

--- index.user.js
+++ index.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         MastodonTranslate
 // @namespace    https://uchuu.io/
-// @version      1.2.0
+// @version      1.3.0
 // @description  Aims to provide a translate interface into Mastodon instances
 // @author       tomo@uchuu.io
 // @match        *://*/web/*
+// @match        *://*/settings/preferences
 // @connect      translate.uchuu.io
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
@@ -28,7 +31,7 @@
                 translateArea.classList.add('toot__translation');
                 translateArea.innerHTML = '<i style="font-style: italic;">Translated:</i> '+translatedText;
 
-                status.querySelectorAll('div.status__content')[0].appendChild(translateArea);
+                status.querySelector('div.status__content').appendChild(translateArea);
             },
             onerror: function() {
                 console.log('There was an error');
@@ -37,10 +40,10 @@
     }
 
     function addTranslateLink(status) {
-        var statusText = status.querySelectorAll('div.status__content')[0].textContent;
-        var dropdown = status.querySelectorAll('div.dropdown__content.dropdown__right ul')[0];
+        var statusText = status.querySelector('div.status__content').textContent;
+        var dropdown = status.querySelector('div.dropdown__content.dropdown__right ul');
 
-        var separator = dropdown.querySelectorAll('li.dropdown__sep')[0];
+        var separator = dropdown.querySelector('li.dropdown__sep');
 
         var listItem = document.createElement('li');
         listItem.classList.add('translate__toot');
@@ -53,12 +56,26 @@
         link.addEventListener('click', function(e) {
             e.preventDefault();
             if (status.querySelectorAll('p.toot__translation').length === 0) {
-                getTranslation(status, 'en', statusText);
+                getTranslation(status, GM_getValue('lang', 'en'), statusText);
             }
         }, false);
 
         listItem.appendChild(link);        
         dropdown.insertBefore(listItem, separator);
+    }
+
+    function saveSettings(event) {
+        if (event.target.tagName.toLowerCase() === 'button' && event.target.textContent === 'Save changes') {
+            event.preventDefault();
+            var input = document.getElementById('translation_locale');
+            var selectedLanguage = input.options[input.selectedIndex].value;
+            GM_setValue('lang', selectedLanguage);
+
+            setTimeout(function() {
+                document.querySelector('body').removeEventListener('click', saveSettings, false);
+                actions.children[0].click();
+            }, 500);
+        }
     }
 
     document.querySelector('body').addEventListener('click', function(event) {
@@ -70,5 +87,29 @@
                 addTranslateLink(status);
             }
         }
-    });
+    }, false);
+
+    if (window.location.pathname === '/settings/preferences') {
+        // We're on the settings page
+        var form = document.querySelector('form.simple_form');
+        var actions = document.querySelector('div.actions');
+
+        var settingsGroup = form.querySelector('div.fields-group').cloneNode(true);
+        settingsGroup.children[1].remove(); // Remove the privacy element from the clone
+
+        var languageDiv = settingsGroup.children[0];
+        languageDiv.classList.remove('user_locale');
+        languageDiv.classList.add('translation_locale');
+        var label = languageDiv.children[0].children[0];
+        label.setAttribute('for', 'translation_locale');
+        label.textContent = 'Translation Language';
+        var input = languageDiv.children[0].children[1];
+        input.setAttribute('name', 'user[translation]');
+        input.setAttribute('id', 'translation_locale');
+        input.value = GM_getValue('lang', 'en');
+
+        form.insertBefore(settingsGroup, actions);
+
+        document.querySelector('body').addEventListener('click', saveSettings, false);
+    }
 })();
